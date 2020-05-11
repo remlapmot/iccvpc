841c0186782b9b7ac9c02e745800ded188a878ab
Tom Palmer <remlapmot@hotmail.com>
2020-05-11 13:44:46 +0100

    update tests

--- tests/testthat/test-icc.R
+++ tests/testthat/test-icc.R
@@ -12,10 +12,15 @@ vc <- lme4::VarCorr(fm1) ## default print method: standard dev and corr
 # print(vc, comp = c("Variance", "Std.Dev."), digits = 2)
 vcdf <- as.data.frame(vc, order = "lower.tri")
 iccfit <- vcdf[1, 4] / (vcdf[1, 4] + vcdf[2, 4])
+
+test_that("Check no error from running icc function", {
+  expect_error(icc(fm1), NA)
+})
+
 iccpkg <- icc(fm1)
 
 test_that("ICC for lme4::VarCorr() helpfile example", {
-  expect_equivalent(iccpkg, iccfit, tol =  1e-3)
+  expect_equivalent(iccpkg$icc, iccfit, tol =  1e-3)
 })
 
 test_that("Pass model of incorrect class to icc()", {
@@ -33,18 +38,23 @@ test_that("Test print method for class iccmlm", {
 })
 
 test_that("Check exactly what is printed", {
-  expect_output(print(iccpkg, digits = 2), "\\nIntra-class correlation coefficient: 0.69")  
+  expect_output(print(iccpkg, digits = 2), 
+                "ICC: 0.69")  
 })
 
 test_that("Check percent option to print gives percent sign at end", {
-  expect_output(print(iccpkg, percent = TRUE, digits = 2), "\\nIntra-class correlation coefficient: 68.57%")  
+  expect_output(print(iccpkg, percent = TRUE, digits = 2),
+                "ICC: 68.57%")  
 })
 
-test_that("Check bootstrap standard error and CI", {
-  btci <- bootci(iccpkg,
-                 fit = fm1,
-                 data = Orthodont,
-                 seed = 20200510)
-  expect_equal(btci$bci[2], .5496791, tol = 1e-4)
-  expect_equal(btci$bci[3], .8217991, tol = 1e-4)
+test_that("Check bootstrap default CI", {
+  iccbci <- icc(fm1, bci = TRUE, seed = 20200510)
+  expect_equal(iccbci$bci[2], .5496791, tol = 1e-4)
+  expect_equal(iccbci$bci[3], .8217991, tol = 1e-4)
+})
+
+test_that("Check bootstrap 90% CI", {
+  iccbci <- icc(fm1, bci = TRUE, seed = 20200510, conf = .9)
+  expect_equal(iccbci$bci[2], .5715540, tol = 1e-4)
+  expect_equal(iccbci$bci[3], .7999, tol = 1e-4)
 })
