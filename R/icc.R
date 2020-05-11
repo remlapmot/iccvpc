5ae317a02894a7fd86e18ccead6b2dce8f56f0fb
Tom Palmer <remlapmot@hotmail.com>
2020-05-11 13:44:25 +0100

    updates for including bootstapping in icc

--- R/icc.R
+++ R/icc.R
@@ -4,10 +4,19 @@
 #' @param bci Logical, indicating whether to return a bootstrap confidence interval for the ICC. The default is `FALSE`.
 #' @param seed Random number seed to make the bootrstrapping reproducible.
 #' @param R The number of bootstrap replicates. The default is `50`.
-#' @param ... Further arguments passed to [boot::boot()] for the bootstrapping.
+#' @param ... Further arguments passed to [boot::boot.ci()] for the bootstrap confidence interval.
 #' 
 #' @export
-#' @return Object of class `iccmlm` containing the ICC.
+#' @return Object of class `iccmlm` containing the following elements:
+#' * `icc` the ICC
+#' * `bci` if specified, the bootstrap confidence interval.
+#' @examples
+#' require(lme4)
+#' # fit random intercept model, with fixed effect for Days
+#' fm1 <- lmer(Reaction ~ Days + (1 | Subject), sleepstudy)
+#' summary(fm1)
+#' icc(fm1)
+#' icc(fm1, bci = TRUE, seed = 12345)
 icc <- function(model, bci = FALSE, seed, R = 50, ...) {
 
   clmod <- class(model)[1]
@@ -31,24 +40,23 @@ icc <- function(model, bci = FALSE, seed, R = 50, ...) {
   intvar <- eval(parse(text = intvarexpr))
   icc <- intvar/(resvar + intvar)  
   
-  class(icc) <- append(class(icc), "iccmlm")
-  
   if (bci) {
     if (!missing(seed)) set.seed(seed)
-    bsrun <- boot::boot(data = model$frame, 
+    bsrun <- boot::boot(data = model@frame, 
                         statistic = iccmodel, 
                         R = R, 
-                        fit = model,
-                        ...)
+                        fit = model)
     bias <- mean(bsrun$t) - bsrun$t0
-    bootci <- boot::boot.ci(bsrun, type = 'norm')
+    bootci <- boot::boot.ci(bsrun, type = 'norm', ...)
     bci <- c(bootci$normal[1], bootci$normal[2:3] + bias)
-  } 
-  else {
+  } else {
     bci <- NULL
   }
   
-  return(list(icc = icc, bci = bci))
+  retlist <- list(icc = icc, bci = bci)
+  class(retlist) <- append(class(retlist), "iccmlm")
+  
+  return(retlist)
 }
 
 #' print method for an object of class iccmlm
@@ -58,17 +66,31 @@ icc <- function(model, bci = FALSE, seed, R = 50, ...) {
 #' @param percent Logical indicating whether the ICC should be reported as a percentage.
 #' @param ... Additional arguments passed to [`print`].
 #' 
+#' @examples 
+#' require(lme4)
+#' fm1 <- lmer(Reaction ~ Days + (1 | Subject), sleepstudy)
+#' summary(fm1)
+#' print(icc(fm1, bci = TRUE, seed = 12345), percent = TRUE, digits = 2)
 #' @export
 print.iccmlm <- function(x, 
                          digits = getOption("digits"), 
                          percent = FALSE, ...) {
-  if (!percent) {
-    cat("\nIntra-class correlation coefficient: ")
-    cat(round(x[1], digits = digits))
+  if (!percent) mult <- 1 else mult <- 100
+  cat("ICC: ")
+  cat(round(x$icc * mult, digits = digits))
+  if (percent) cat("%")
+  if (!is.null(x$bci)) {
+    cat(" (")
+    cat(round(x$bci[1] * 100, digits = 0))
+    cat("% CI: ")
+    cat(round(x$bci[2] * mult, digits = digits))
+    if (percent) cat("%")
+    cat(", ")
+    cat(round(x$bci[3] * mult, digits = digits))
+    if (percent) cat("%")
+    cat(")\n")
   } else {
-    cat("\nIntra-class correlation coefficient: ")
-    cat(round(x[1] * 100, digits = digits))
-    cat("%")
+    cat("\n")
   }
   invisible(x)
 }
@@ -83,5 +105,5 @@ print.iccmlm <- function(x,
 iccmodel <- function(data, indices, fit){
   d <- data[indices, ]
   fit <- update(fit, data = d)
-  icc(fit)
+  icc(fit)$icc
 }
